86ce1851b6dc806ec1d28859891973b735421bb3
1Mike21 <mihail.sokolov21@yandex.ru>
2023-10-15 21:56:55 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+let center = [55.7667056879933, 37.60289606627276]; 
+function init() {
+  let map = new ymaps.Map('map', {
+    zoom: 16,
+    center: center
+  });
+
+  let MusicHouse = new ymaps.Placemark(center, {
+    iconLayout: 'default#image',
+    iconImageSize: [64,74],
+    iconImageOffset: [0,0]
+  });
+
+  map.geoObjects.add(MusicHouse);
+}
+ymaps.ready(init);
